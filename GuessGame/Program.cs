4281b1f3d7cda10cb81792528c7cd11e2dcf7a5c
Anna Dereniuk <Anna_Dereniuk@epam.com>
2019-03-01 17:38:28 +0200

    game

---
+++ GuessGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndm = new Random();
+            int valueToGuess = rndm.Next(1, 3);
+            Console.WriteLine("Guess a number from 1 to 20");
+            String s = Console.ReadLine();
+            int value = Convert.ToInt32(s);
+            while (value != valueToGuess)
+            {
+                Console.WriteLine("Oops, this is not my value, try again");
+                s = Console.ReadLine();
+
+                value = Convert.ToInt32(s);
+
+            }
+            Console.WriteLine("Success! Nostradamus is jealous of you!");
+            Console.ReadKey();
+        }
+    }
+}
