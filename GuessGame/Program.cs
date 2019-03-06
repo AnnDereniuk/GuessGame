49806b203d9968ade2fd47d0dee9d8d0b4dad30a
Anna Dereniuk <Anna_Dereniuk@epam.com>
2019-03-06 13:40:11 +0200

    fixed due to the task

--- GuessGame/Program.cs
+++ GuessGame/Program.cs
@@ -7,18 +7,24 @@ namespace GuessGame
         static void Main(string[] args)
         {
             Random rndm = new Random();
-            int valueToGuess = rndm.Next(1, 3);
+            int valueToGuess = rndm.Next(1,21);
             Console.WriteLine("Guess a number from 1 to 20");
             String s = Console.ReadLine();
             int value = Convert.ToInt32(s);
             while (value != valueToGuess)
             {
-                Console.WriteLine("Oops, this is not my value, try again");
-                s = Console.ReadLine();
+                Console.WriteLine("Oops, this is not my value");
+                if (value > valueToGuess)
+                {
+                    Console.WriteLine("Your value is greater than mine, try again");
+                }
+                else Console.WriteLine("Your value is lower than mine, try again");
 
+                s = Console.ReadLine();
                 value = Convert.ToInt32(s);
 
             }
+
             Console.WriteLine("Success! Nostradamus is jealous of you!");
             Console.ReadKey();
         }
